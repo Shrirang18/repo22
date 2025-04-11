933df6d91d704f7df1ce88eb917e75d4d00d027f
Shrirang18 <samrougeofficial@gmail.com>
2025-04-11 09:50:43 +0530

    save file

---
+++ person.java
@@ -0,0 +1,17 @@
+class Person {
+    String name;
+    int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("John Doe", 30);
+        person1.displayInfo();
+    }
+}
